3ef678caac3f25a23fad82335631fab219259e53
marinppetkov <marin.petkov@mail.bg>
2023-10-25 14:55:44 +0300

    fix attribute

--- init.sh
+++ init.sh
@@ -8,7 +8,7 @@ apt-get -y install binutils
 git clone https://github.com/aws/efs-utils
 cd efs-uti
 ./build-deb.sh
-sudo apt-get -y install ./build/amazon-efs-utils*deb
+apt-get -y install ./build/amazon-efs-utils*deb
 cd ..
-sudo mount -t efs -o tls ${var.efs_dns}:/ /var/www/html
-echo \"${var.efs_dns}:/ /var/www/html efs _netdev,noresvport,tls 0 0\" | sudo tee -a /etc/fstab
+mount -t efs -o tls ${var.efs_dns}:/ /var/www/html
+echo \"${var.efs_dns}:/ /var/www/html efs _netdev,noresvport,tls 0 0\" | tee -a /etc/fstab
